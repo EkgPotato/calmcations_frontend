d85c9997319bd14d189ca8ecd1c7c515391049e5
Konrad Trochim <EkgScam@gmail.com>
2025-10-04 23:07:11 +0200

    Update app.service.js

--- src/app.service.js
+++ src/app.service.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000'; 
+const API_BASE_URL = 'http://hackyeahtraveltask-production.up.railway.app'; 
 
 async function handleResponse(response) {
     if (!response.ok) {
